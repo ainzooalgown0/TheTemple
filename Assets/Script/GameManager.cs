30d3518bff45a8de0c40a7cbfed55fec2b120f73
お好み焼きネタ <>
2018-12-09 02:11:15 +0900

    first commit

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class GameManager : MonoBehaviour {
+
+	//定数定義
+	private const int MAX_ORB = 10;     //オーブ最大数
+    private const int RESPAWN_TIME = 1; //オーブが発生する秒数
+    private const int MAX_LEVEL = 2; //最大お寺レベル
+
+	//オブジェクト参照
+    public GameObject orbPrefab;        //オーブプレハブ
+    public GameObject smokePrefab;      //煙プレハブ
+    public GameObject kusudamaPrefab;   //くす玉プレハブ
+	public GameObject canvasGame;       //ゲームキャンバス   
+    public GameObject textScore;        //スコアテキスト
+    public GameObject imageTemple;      //お寺
+
+    //メンバ変数
+    private int score = 0;          //現在のスコア
+    private int nextScore = 10;    //レベルアップまでに必要なスコア
+
+    private int currentOrb = 0;     //現在のオーブ数
+
+    private int templeLevel = 0;    //お寺のレベル
+
+    private DateTime lastDateTime;  //前回オーブを生成した時間
+
+    private int[] nextScoreTable = new int[] { 10, 10, 10 };    //レベルアップ値
+
+
+	// Use this for initialization
+	void Start () 
+    {
+
+        currentOrb = 10;
+
+        // 初期オーブ生成
+        for (int i = 0; i < currentOrb; i++)
+        {
+            CreateOrb();
+        }
+
+        //初期設定
+        lastDateTime = DateTime.UtcNow;
+        nextScore = nextScoreTable[templeLevel];
+        imageTemple.GetComponent<TempleManager>().SetTemplePicture(templeLevel);
+        imageTemple.GetComponent<TempleManager>().SetTempleScale(score, nextScore);
+
+        RefreshScoreText();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("デバッグスタート！！");
+
+        if(currentOrb < MAX_ORB)
+        {
+            TimeSpan timeSpan = DateTime.UtcNow - lastDateTime;
+
+            if(timeSpan >= TimeSpan.FromSeconds (RESPAWN_TIME))
+            {
+                while (timeSpan >= TimeSpan.FromSeconds(RESPAWN_TIME))
+                {
+                    CreateNewOrb();
+                    timeSpan -= TimeSpan.FromSeconds(RESPAWN_TIME);
+                }
+            }
+        }
+    }
+
+    //新しいオーブを生成
+    public void CreateNewOrb()
+    {
+        lastDateTime = DateTime.UtcNow;
+        if (currentOrb >= MAX_ORB)
+        {
+            return;
+        }
+        CreateOrb();
+        currentOrb++;
+    }
+
+    //オーブ生成
+    public void CreateOrb ()
+    {
+        GameObject orb = (GameObject)Instantiate(orbPrefab);
+        orb.transform.SetParent(canvasGame.transform, false);
+        orb.transform.localPosition = new Vector3
+            (
+                UnityEngine.Random.Range(-300.0f, 300.0f),
+                UnityEngine.Random.Range(-140.0f, -500.0f),
+                0f
+            );
+    }
+
+    //　オーブ入手
+    public void GetOrb ()
+    {
+        score += 1;
+
+        //レベルアップ値をこえないよう制限
+        if(score > nextScore)
+        {
+            score = nextScore;
+        }
+
+        TempleLevelUp();
+
+        RefreshScoreText();
+
+        imageTemple.GetComponent<TempleManager>().SetTempleScale(score, nextScore);
+
+        //ゲームクリア判定
+        if((score == nextScore) && (templeLevel == MAX_LEVEL))
+        {
+            ClearEffect();
+        }
+
+        currentOrb--;
+    }
+
+    void RefreshScoreText()
+    {
+        textScore.GetComponent< Text > ().text = "徳:" + score + " / " + nextScore;
+    }
+
+    //寺のレベル管理
+    void TempleLevelUp()
+    {
+        if(score >= nextScore)
+        {
+            if(templeLevel < MAX_LEVEL)
+            {
+                templeLevel++;
+
+                score = 0;
+
+                TempleLevelUpEffect();
+
+                nextScore = nextScoreTable[templeLevel];
+                imageTemple.GetComponent<TempleManager>().SetTemplePicture(templeLevel);
+            }
+
+        }
+    }
+
+    void TempleLevelUpEffect()
+    {
+        GameObject smoke = (GameObject)Instantiate(smokePrefab);
+        smoke.transform.SetParent(canvasGame.transform, false);
+        smoke.transform.SetSiblingIndex(2);
+
+        Destroy(smoke, 0.5f);
+    }
+
+    //寺が最後まで育った時の演出
+    void ClearEffect()
+    {
+        GameObject kusudama = (GameObject)Instantiate(kusudamaPrefab);
+        kusudama.transform.SetParent(canvasGame.transform, false);
+    }
+}
+
+internal class レベルアップまでに必要なスコア
+{
+}
---
+++ Assets/Script/OrbManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OrbManager : MonoBehaviour
+{
+
+    //　オブジェクト参照
+    private GameObject gameManager; //ゲームマネジャー
+
+    // Use this for initialization
+    void Start()
+    {
+
+        Debug.Log("デバッグスタート！！");
+
+        gameManager = GameObject.Find("GameManager");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //string filename = "test.jpg";
+        //testZikan(filename);
+        Debug.Log("デバッグスタート！！");
+    }
+
+    //オーブ取得
+    public void TouchOrb()
+    {
+        if (Input.GetMouseButton(0) == false)
+        {
+            return;
+        }
+
+        gameManager.GetComponent<GameManager>().GetOrb();
+        Destroy(this.gameObject);
+    }
+
+
+//    public void buttonClick()
+//    {
+//#if UNITY_ANDROID
+//        // Textコンポーネント郡を取得します。
+//        var components = this.gameObject.GetComponentsInChildren<Text>();
+//        // テキストを文字の状態によって変更するようにします。
+//        components[0].text = comp
+//                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       onents[0].text == "Button" ? "Changed" : "Button";
+
+
+
+//#endif
+//    }
+
+//    public void testZikan(filename)
+//    {
+//#if UNITY_ANDROID
+//        string fullPath = "jar:file://" + Application.dataPath + "/!/assets/" + filename;
+//        WWW www = new WWW(fullPath);
+//        while (!www.isDone)
+//        {
+//        }
+//        FilePath = Application.persistentDataPath + filename;
+//        File.WriteAllBytes(toPath, www.bytes);
+//#endif
+    //}
+}
